45d5cb7b5076b702191dfc4f2fed4ed4f4e00d21
Albert Augustine <145218393+Pyro-Warrior-1884@users.noreply.github.com>
2024-09-13 21:23:27 +0530

    Update Find_All_Leader_Array.py

--- Find_All_Leader_Array.py
+++ Find_All_Leader_Array.py
@@ -3,9 +3,10 @@ arr = [0 for _ in range(n)]
 for i in range(n):
     arr[i] = int(input())
 
-max_num = 0 
+max_num = arr[len(arr)-1]
 leader = []
-for i in range(len(arr)-1,-1,-1):
+leader.insert(0, max_num)
+for i in range(len(arr)-2,-1,-1):
     if max_num < arr[i]:
         max_num = arr[i]
         leader.insert(0, arr[i])
